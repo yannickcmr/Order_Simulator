4abe24e7d02ea3626e240ae7fa44100a6ac68742
Yannick <y.ciomer@web.de>
2022-10-31 20:34:25 +0100

    updated some functions

--- Personen_abc.py
+++ Personen_abc.py
@@ -1,5 +1,5 @@
-from numpy import str_
-from Produktklassen_abc import main as main_product 
+import io, os, sys, codecs, time
+import random as rd
 from abc import ABC, abstractmethod
 
 """
@@ -8,29 +8,35 @@ Name: str, Customer_ID: int, Sale: float in [0,1], Email: str,
 Telephone: int with S.N. in front, i.e. (+)49, 
 """
 
+Path_Saving = "Test/"
+
+try:
+    sys.stdin = codecs.getreader("utf-8")(sys.stdin.detach())
+    sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+except:
+    pass  
+
 class Person(ABC):
 	"Simple class for all the people, who you need to take track of."
 	Dictonary = {}
 
-	# Name of the person. 
 	def Name(self):
 		return self.name
 
-	# Date of birth
 	def Birthday(self):
 		return self.birthday
 
-	# Street/no. and city of residency
 	def Residence(self):
 		return self.residence
 
-	# Email adress
 	def Email(self):
 		return self.email
 
-	# Mobil number. 
 	def Telephone(self):
 		return self.telephone 
+
+	def Last_Order(self):
+		return self.last_order
 
 	# Generall infos about that person.
 	@abstractmethod
@@ -41,13 +47,6 @@ class Person(ABC):
 	# Assiged Customer or Employee ID. 
 	def Person_ID(self):
 		pass
-
-	@abstractmethod
-	def Discount(self):
-		pass
-
-	def Last_Order(self):
-		return self.last_order
 
 class Customer(Person):
 	"Someone, who is just a custumer to the company"
@@ -61,25 +60,19 @@ class Customer(Person):
 		self.email = email
 		self.telephone = telephone
 		self.last_order = None
-		self.favourite_item = None
-		self.discount_seeker = False
-		self.trend_seeker = False
-		self.mean_volume = None
-		self.mean_order = None
-		self.mean_items = None
+		self.mean_volume: int = 0
+		self.mean_order: int = 0
+		self.mean_items: int = 0
 
 	def Person_ID(self) -> int:
 		return self.customer_id
-
-	def Discount(self):
-		return None
 
 	def Infos(self) -> list:
 		return [self.name, self.customer_id, self.email, self.telephone]
 
 class Employee(Person):
 	"Employee of the company."
-	Dictonary = {"L_J_001": None, "Y_C_002": None, "M_S_003": None, }
+	Dictonary = {"L_J_001": None, "Y_C_002": None, "M_S_003": None }
 
 	def __init__(self, name: str, birthday: str = None, residence: str = None, employee_id: int = 0, discount :float = 0, position: str = None, email: str = None,  telephone: int = 0, employed_since: str = None):
 		self.name = name
@@ -107,3 +100,47 @@ class Employee(Person):
 
 	def Employed_Since(self) -> str:
 		return self.employed_since
+
+
+def Read_CSV(file: str) -> list:
+	file_path = os.path.join(Path_Saving, file)
+
+	with io.open(file_path, "r", encoding="utf-8") as file_names:
+		lines = file_names.readlines()
+		first_names = [*lines[0].translate({ord("\n"): None}).split(";")]
+		last_names = [*lines[1].translate({ord("\n"): None}).split(";")]
+		cities = [*lines[2].translate({ord("\n"): None}).split(";")]
+		mail = [*lines[3].translate({ord("\n"): None}).split(";")]
+	
+	return first_names, last_names, cities, mail
+
+def Create_Customer(first_: list, last_: list, cities: list, mails: list) -> Customer:
+	name = f"{rd.choice(first_)} {rd.choice(last_)}"
+	city = rd.choice(cities)
+	# format: dd.mm.yyyy
+	birthday = f"{rd.randint(1,31)}.{rd.randint(1,12)}.{rd.randint(1960,2005)}"
+	# country code and random phone number.
+	phone = f"+{rd.randint(1,60)} {rd.randint(1000000, 99999999)}"
+	# spliting name into first and last for mail adress.
+	cache = name.split(" ")
+	# using first letter of the first name and the last name all in lower case.
+	mail = f"{cache[0][0].lower()}.{cache[1].lower()}@{rd.choice(mails)}"
+	return Customer(name, birthday, city, hash(name), mail, phone)
+
+def Randomize_Names(file: str, set_size: int) -> list:
+	# get list of variables.
+	first_names, last_names, cities, mails = Read_CSV(file)
+	return [Create_Customer(first_names, last_names, cities, mails) for i in range(0, set_size)]
+
+if __name__ == "__main__":
+	start = time.perf_counter()
+	test_names = "Person.csv"
+	set_size = 150
+
+	first_, last_, cities, mail = Read_CSV(test_names)
+	print(f"Testing Read_CSV:\nfirst names: {first_}\nlast names: {last_}\ncities: {cities}\nmail: {mail}\n")
+	random_names = Randomize_Names(test_names, set_size)
+	print(f"Testing Randomize_Names:\n")
+	for customer in random_names:
+		print(f"{customer.birthday}\t\t{customer.telephone}\t\t{customer.name}\t\t{customer.email}\t\t{customer.customer_id} ")
+	print(f"Done generating {set_size} in {time.perf_counter() - start} sec.")
