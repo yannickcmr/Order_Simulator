070600f25400d6ed27d0b676705fcfda83c9f7a7
Yannick <y.ciomer@web.de>
2022-10-31 21:20:57 +0100

    updated fetch

--- Fetch_Products.py
+++ Fetch_Products.py
@@ -21,28 +21,32 @@ def convert_price(price: str) -> float:
 def get_page_selenium(url):
     driver = webdriver.Chrome(path_driver)
     driver.get(url)
+
     # parse page via htmlparser.
     parser = HTMLParser()
     parser.feed(driver.page_source)
     page_html = driver.page_source
+
     # quit driver to close tab.
     driver.quit()
     return page_html
+
 
 class Product_Driver:
     """ Enter product that should be scraped """
     def __init__(self, category: str, url: str, perf: bool = False) -> None:
         self.category = category
         self.url = url
+        self.time = None
+
         # if you want to see the time, use perf = True.
-        if perf: 
-            self.perf = perf
-            self.time = time.perf_counter()
+        if perf: self.time = time.perf_counter()
+
         # read page via selenium.
         page = get_page_selenium(self.url)  
         self.product_content = BS(page, features="html.parser")
-    
-        if self.perf: print(f"read page in {time.perf_counter() - self.time} sec")
+
+        if self.time != None: print(f"read page in {time.perf_counter() - self.time} sec")
 
     # get title from the product page.
     def Get_Title(self):
@@ -55,6 +59,7 @@ class Product_Driver:
             self.name = "None"
             print("couldn't catch title")
             return None
+
         # add title to Product.
         self.name = title_cache.text.translate({ord("ß"):"ss"}).strip()
         self.hash = hash(self.url)
@@ -69,9 +74,11 @@ class Product_Driver:
             rating_cache = rating_cache.findAll("table", id="histogramTable")[1].findAll("tr")
             for stars in rating_cache:
                 rating_distribution.append(int(nums.findall(stars.text)[1]))
+
         # get number of total ratings. 
         rating_cache = self.product_content.find("div", {"class": "a-row a-spacing-medium averageStarRatingNumerical"}).text
         total_rating = int(nums.findall(rating_cache)[0].translate({ord(","): None}))
+        
         # set product.rating to the before mentioned format.
         self.rating = [total_rating, rating_distribution]
 
@@ -82,22 +89,22 @@ class Product_Driver:
         if price_cache == None:
             price_cache = self.product_content.find("table", {"class": "a-lineitem a-align-top"})
         price_pay = price_cache.find("span", {"class": "a-offscreen"})
+
         # convert price.
         self.price = convert_price(price_pay.text)
+
         # if there is a discount, then it will be added.
         price_sugg = price_cache.find("span", {"class": "a-size-small a-color-secondary aok-align-center basisPrice"})
-        if price_sugg == None: 
-            self.sugg_price = 0
-        else: 
-            self.sugg_price = convert_price(price_sugg.text)
+        if price_sugg == None: self.sugg_price = 0
+        else: self.sugg_price = convert_price(price_sugg.text)
 
     def Get_Tags(self):
-        # split title.
-        comp_title = self.name.split(" ")
         # get words in title.
+        comp_title = self.name.split(" ")
         comp_title = [letters.findall(x) for x in comp_title]
         comp_title = [x[0] for x in comp_title if len(x) > 0]
         comp_title = ",".join(comp_title)
+
         # reduce tags.
         comp_title = str(reduce_tags(comp_title)).strip("[]").translate({ord("'"): None}).translate({ord(" "): None})
         self.tags = comp_title
@@ -123,24 +130,28 @@ class Product_Driver:
     def Computer_Ram(self):
         pass
 
-    def Get_Data(self):
-        # get common attributes
-        self.Get_Title()
-        self.Get_Prices()
-        self.Get_Rating_Info()
-        self.Get_Tags()
-
-        # get product specific attribute.
-        if self.perf: print(f"got attributes in {time.perf_counter() - self.time}")
+    def Get_Special_Attr(self):
         if self.category== "Smartphone": self.Smartphone_Size()
         elif self.category  == "Headphone": self.Headphone_NC()
         elif self.category  == "TV": self.TV_Size()
         elif self.category == "Computer": self.Computer_Ram()
 
-        if self.perf: print(f"finished in {time.perf_counter() - self.time}")
+    def Get_Data(self):
+        self.Get_Title()
+        self.Get_Prices()
+        self.Get_Rating_Info()
+        self.Get_Tags()
+        self.Get_Special_Attr()
+
+        if self.time != None: print(f"-> got attributes in {time.perf_counter() - self.time}")
 
 if __name__ == "__main__":
     start = time.perf_counter()
     url_test = "https://www.amazon.de/MPVA3ZD-A/dp/B0BDJC83QY/ref=sr_1_1_sspa?crid=3MXI5609W6NW2&keywords=iphone&qid=1667246474&qu=eyJxc2MiOiI2LjgzIiwicXNhIjoiNi45OCIsInFzcCI6IjYuNDgifQ%3D%3D&sprefix=iphon%2Caps%2C142&sr=8-1-spons&psc=1"
     test = Product_Driver("Smartphone", url_test, True)
+    test.Get_Data()
+    print(f"Product:")
+    for var in vars(test):
+        if var != "product_content":
+            print(f"{var}: {vars(test)[var]}")
     print(f"Done scraping products in {time.perf_counter() - start} sec.")
